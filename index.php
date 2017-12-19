365e5f404b86c28116f78d4d42197d878897cd23
Ruhum (era) <34663542+Ruhum36@users.noreply.github.com>
2017-12-19 20:13:33 +0300

    Upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Php File Upload</title>
+	<style type="text/css">
+		
+		*{
+			font-family: arial;
+		}
+		#FileName{
+			padding: 10px; width: 450px; border-radius: 3px; border: 1px solid #ddd;
+		}
+
+	</style>
+</head>
+<body>
+
+<div>
+	
+	<center><h2>Php File Upload</h2></center>
+ 
+	<form action="" method="POST" style="width: 585px; margin: auto;" enctype="multipart/form-data">
+			
+		<input type="file" name="File" id="FileName" />
+		<input type="submit" name="FileUpload" value="Upload File" style="padding: 12px; width: 100px; border-radius: 3px; border: 1px solid #ddd; background-color: #f6f6f6; margin-top: 10px;" /><br /><br />
+
+	</form>
+
+</div>
+
+<?php
+	
+	
+	if(isset($_POST['FileUpload'])){
+
+		$DirFolder = './files/';
+		$UploadFile = $DirFolder. basename($_FILES['File']['name']);
+
+		if (move_uploaded_file($_FILES['File']['tmp_name'], $UploadFile)){
+
+		    echo "<center>File is valid, and was successfully uploaded.</center>";
+
+		} else {
+
+		    echo "<center>Possible file upload attack!</center>";
+
+		}
+
+	}
+
+?>
+
+</body>
+</html>
